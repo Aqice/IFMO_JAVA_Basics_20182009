4716fcf8f56f1e5d967c1c6aa932d6bb958bc0b2
Aqice <Aqice26@gmail.com>
2018-09-26 23:14:25 +0300

    P3222 Oreshin Svyatoslav task-3 Lambdas and streams

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,7 +1,14 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.readAllLines;
 
 
 public class WarAndPeaceExercise {
@@ -15,7 +22,36 @@ public class WarAndPeaceExercise {
         // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
         // TODO Also omit any word with lengths less than 4 and frequency less than 10
 
-        throw new UnsupportedOperationException();
+        String data = "";
+        try {
+            data += readAllLines(tome12Path, Charset.forName("windows-1251")).stream().collect(Collectors.joining(" "));
+            data += readAllLines(tome34Path, Charset.forName("windows-1251")).stream().collect(Collectors.joining(" "));
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        data = data.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase();
+        String[] words = data.split(" ");
+        Stream <String> wordStream = Stream.of(words);
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        wordStream.forEach(
+                (String word) -> map.put(word, map.getOrDefault(word,0) + 1)
+        );
+
+        List<Map.Entry<String,Integer>> sortedMapList = new ArrayList<>(map.entrySet());
+
+        Collections.sort(sortedMapList, (o1, o2) ->
+                (o1.getValue().equals(o2.getValue())) ? o1.getKey().compareTo(o2.getKey())
+                        : o2.getValue().compareTo(o1.getValue()));
+
+        return sortedMapList.stream()
+                .filter(item -> item.getValue() >= 10 && item.getKey().length() >= 4)
+                .map(item -> item.getKey() + " - " + item.getValue())
+                .collect(Collectors.joining("\n"));
     }
 
 }
